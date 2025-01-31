5fb1691d5912704204a9a160b9ca31e86b6c2687
os2594 <ocarboneylopez@unomaha.edu>
2025-01-31 10:04:05 -0600

    feat: api de pagina html con node

--- Clase2/index.js
+++ Clase2/index.js
@@ -1,1 +1,12 @@
-console.log("Hola");
+const http = require('http');
+
+const server = http.createServer((req, res) => {
+    const contentResponse = '<html><body>Hello desde server con node</body></html>'
+    res.write(contentResponse);
+    res.end();
+});
+
+server.listen(5500);
+
+console.log("servidor esta levantado en el puerto: ", 5500);
+
